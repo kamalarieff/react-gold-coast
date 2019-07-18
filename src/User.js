7a45081024d834de283b7d488e941f905d8dbad4
Kamal Arieff <kamal.arieff@gmail.com>
2019-07-19 02:06:21 +0800

    Change user list to table

--- src/User.js
+++ src/User.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import Typography from "@material-ui/core/Typography";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 import styled from "@emotion/styled";
 import Icon from "@mdi/react";
 import { mdiCloseCircle, mdiCheckCircle } from "@mdi/js";
@@ -24,28 +26,19 @@ const IconStyled = styled(Icon)`
   vertical-align: middle;
 `;
 
-const UserHeader = () => (
-  <ListItem>
-    <ListItemText primary={<Typography variant="body1">Name</Typography>} />
-    <ListItemText
-      primary={<Typography variant="body1">Flight ticket</Typography>}
-    />
-  </ListItem>
-);
-
 const UserItem = ({ username, isFlightTicketPurchased }) => (
-  <ListItem>
-    <ListItemText primary={username} />
-    <ListItemText
-      primary={
-        isFlightTicketPurchased ? (
-          <IconStyled path={mdiCheckCircle} size={1} color="green" />
-        ) : (
-          <IconStyled path={mdiCloseCircle} size={1} color="red" />
-        )
-      }
-    />
-  </ListItem>
+  <TableRow>
+    <TableCell>
+      <Typography variant="body1">{username}</Typography>
+    </TableCell>
+    <TableCell align="center">
+      {isFlightTicketPurchased ? (
+        <IconStyled path={mdiCheckCircle} size={1} color="green" />
+      ) : (
+        <IconStyled path={mdiCloseCircle} size={1} color="red" />
+      )}
+    </TableCell>
+  </TableRow>
 );
 
 const Users = () => (
@@ -54,20 +47,32 @@ const Users = () => (
       <Card
         title="Users"
         render={() => (
-          <List>
-            <UserHeader />
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="body1">Name</Typography>
+                </TableCell>
+                <TableCell align="center">
+                  <Typography variant="body1">Flight ticket</Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
             {!loading ? (
               data.users.map(user => (
-                <UserItem
-                  key={user.id}
-                  username={user.username}
-                  isFlightTicketPurchased={user.purchaseFlightTicket}
-                />
+                <React.Fragment key={user.id}>
+                  <TableBody>
+                    <UserItem
+                      username={user.username}
+                      isFlightTicketPurchased={user.purchaseFlightTicket}
+                    />
+                  </TableBody>
+                </React.Fragment>
               ))
             ) : (
               <p>Loading ...</p>
             )}
-          </List>
+          </Table>
         )}
       />
     )}
