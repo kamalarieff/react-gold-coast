d4837b432e6a1e0d6a4cb65c4a5689bce5ac3bd9
Kamal Arieff <kamal.arieff@gmail.com>
2019-08-05 04:53:47 +0800

    output the budget to html

--- src/Budget.js
+++ src/Budget.js
@@ -10,70 +10,7 @@ import Card from "./hoc/Card";
 import { StaticDataContext } from "./context";
 import { GET_USERS } from "./index";
 
-const budget = [
-  {
-    name: "food",
-    value: 150,
-    currency: "AUD"
-  },
-  {
-    name: "visa",
-    value: 57,
-    currency: "RM"
-  },
-  {
-    name: "dream world",
-    value: 160,
-    currency: "RM"
-  },
-  {
-    name: "movie world + paradise country",
-    value: 186,
-    currency: "RM"
-  },
-  {
-    name: "flight ticket",
-    value: 1343,
-    currency: "RM"
-  },
-  {
-    name: "airbnb",
-    value: 467.63,
-    currency: "RM"
-  },
-  {
-    name: "car rental",
-    value: 115.48,
-    currency: "RM"
-  },
-  {
-    name: "fuel",
-    value: 72.23,
-    currency: "RM"
-  }
-];
-
 const currencyExchange = 2.9;
-
-const transformations = {
-  name: R.identity,
-  value: R.multiply(currencyExchange),
-  currency: R.replace("AUD", "RM")
-};
-
-const covertToRM = R.map(
-  R.when(
-    R.propSatisfies(x => x === "AUD", "currency"),
-    R.evolve(transformations)
-  )
-);
-
-const calculateTotal = R.pipe(
-  R.map(R.prop("value")),
-  R.sum
-);
-
-const budgetInRM = covertToRM(budget);
 
 const TableHeader = () => (
   <TableHead>
@@ -84,16 +21,15 @@ const TableHeader = () => (
   </TableHead>
 );
 
-const TotalRow = () => (
+const TotalRow = ({ total }) => (
   <TableRow style={{ backgroundColor: "lightgrey" }}>
     <TableCell>Total</TableCell>
-    <TableCell>RM {calculateTotal(budgetInRM)}</TableCell>
+    <TableCell>RM {total}</TableCell>
   </TableRow>
 );
 
 const Budget = () => {
   const { store } = useContext(StaticDataContext);
-  console.log("TCL: Budget -> store", store);
   return (
     <Query query={GET_USERS}>
       {({ data: { users } }) => {
@@ -125,38 +61,39 @@ const Budget = () => {
           })
         );
 
-        console.log(
-          "convertToRM",
-          R.map(
-            R.pipe(
-              convertToRM,
-              dividePerPerson
-            )
-          )(store)
+        const budget = R.map(
+          R.pipe(
+            convertToRM,
+            dividePerPerson
+          )
+        )(store);
+
+        const total = R.pipe(
+          R.map(R.path(["data", "value"])),
+          R.sum
+        )(budget);
+
+        return (
+          <Card
+            title="Budget"
+            render={() => (
+              <Table>
+                <TableHeader />
+                <TableBody>
+                  {budget.map(({ name, data: { value } }) => (
+                    <TableRow key={name}>
+                      <TableCell>{name}</TableCell>
+                      <TableCell>RM {value}</TableCell>
+                    </TableRow>
+                  ))}
+                  <TotalRow total={total} />
+                </TableBody>
+              </Table>
+            )}
+          />
         );
-        return <p>Test</p>;
       }}
     </Query>
-  );
-
-  return (
-    <Card
-      title="Budget"
-      render={() => (
-        <Table>
-          <TableHeader />
-          <TableBody>
-            {R.map(x => (
-              <TableRow key={x.name}>
-                <TableCell>{x.name}</TableCell>
-                <TableCell>RM {x.value}</TableCell>
-              </TableRow>
-            ))(budgetInRM)}
-            <TotalRow />
-          </TableBody>
-        </Table>
-      )}
-    />
   );
 };
 
